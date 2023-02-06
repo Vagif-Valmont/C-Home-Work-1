caa74131bd5216423e65e789266bf3fa88085d20
Vagif_Valmont <elekorp@yandex.ru>
2023-02-06 10:52:55 +0300

    Добавил решение задачи 4 семинар 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// ДЗ1 Задача 4 программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+ Console.WriteLine("Ввидите 1 число");
+    int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввидите 2 число");
+    int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввидите 3 число");
+    int c = int.Parse(Console.ReadLine());
+        var Max = a;
+        if (Max < b)
+    {
+            Max = b;
+        if (Max < c)
+    {
+            Max = c;
+Console.WriteLine("Max = " + Max);
+    }   
+    else
+    {
+        Console.WriteLine("Max = " + Max);
+    }
+    } 
+    else if (Max < c) 
+{
+    Max = c; 
+Console.WriteLine("Max = " + Max);
+}
+    else 
+Console.WriteLine("Max = " + Max);
+
