7a4117d868bbd42f0815da1f2e516c107b6c06be
Vagif-Valmont <123108087+Vagif-Valmont@users.noreply.github.com>
2023-02-13 01:22:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -28,3 +28,42 @@ Console.WriteLine("Max = " + Max);
     else 
 Console.WriteLine("Max = " + Max);
 
+// Семинар 2 Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+ int num = new Random().Next(100, 1000);
+ {
+Console.WriteLine(num);
+int a = num / 10 % 10;
+Console.WriteLine(a);
+}
+
+// Семинар 1 Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0)
+{
+   Console.WriteLine("Число чётное"); 
+}
+else
+{
+    Console.WriteLine("Число нечётное"); 
+}
+
+// Семинар 1 Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("your number = ");
+int N = int.Parse(Console.ReadLine());
+var i = 1;
+while ( i <= N)
+{
+    if (i % 2 == 0)
+    {   
+    System.Console.WriteLine(i + ", ");
+    i++;
+    }
+    else
+    {
+        i++;
+    }
+}
